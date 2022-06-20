08c55613db02e87b7a28307f211281e8b36c8830
Irreq <irreq@protonmail.com>
2022-06-20 11:23:19 +0200

    added src

---
+++ src/server.c
@@ -0,0 +1,14 @@
+// File: data-transfer/src/server.c
+// Authors: Irreq, 
+
+#include <unistd.h>
+#include <stdio.h>
+
+void demo_usage() {
+    printf("GPUDirect RDMA server demo\n\n");
+    printf("rdma_server: ");
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
