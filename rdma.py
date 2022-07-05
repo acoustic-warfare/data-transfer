eeb5298be35a2443b3a4b7fd7d8ff6ef0f9b289e
Irreq <irreq@protonmail.com>
2022-07-05 15:28:12 +0200

    removed reduntant file

--- rdma.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from dask_cuda import LocalCUDACluster
-from distributed import Client
- 
-cluster = LocalCUDACluster(protocol="tcp")
-client = Client(cluster)
